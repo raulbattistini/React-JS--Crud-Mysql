03b76677d3e0e4a128c00c366ceaa2a27f0cb84d
Raul Battistini <raulbattistini.3@gmail.com>
2022-07-29 10:55:45 -0300

    almost typed

--- client/App.tsx
+++ client/App.tsx
@@ -1,110 +1,107 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "React";
 import Axios from "axios";
 import React from "React";
-import Card from './src/components/Card'
+import Card from "./src/components/Card";
+import { Formik, Form } from "formik";
+import { api } from "./src/services/api";
 
 export interface IValues {
   id: number;
   name: string;
   cost: string;
   category: string;
-  setValues: React.SetStateAction<any>
-}
-
-interface IList {
-  open: boolean;
-  setOpen: Dispatch<SetStateAction<boolean>>;
-  title: string;
-  id: number;
-  name: string;
-  cost: string;
-  category: string;
-  listCard: IValues[];
-  setListCard: React.SetStateAction<IValues>;
+  setValues: React.SetStateAction<any>;
+  setListCard: SetStateAction<IValues[] | undefined>;
 }
 
 export default function App() {
   const [values, setValues] = useState<IValues>();
-  const [listCard, setListCard] = useState<IList[]>();
+  const [listCard, setListCard] = useState<IValues[]>();
   console.log(listCard);
-  const handleRegisterGame = () => {
-    Axios.post("http://localhost:3001/register", {
-      name: values?.name,
-      cost: values?.cost,
-      category: values?.category,
-    }).then(() => {
-      Axios.post("http://localhost:3001/search", {
-        name: values!.name,
-        cost: values!.cost,
-        category: values?.category,
-      }).then((response) => {
-        setListCard([
-          ...listCard,
-          {
-            id: response.data[0].id,
-            name: values?.name,
-            cost: values?.cost,
-            category: values?.category,
-          },
-        ]);
-      });
-    });
+
+  const handleSubmit = async (values) => {
+    const addPost = {
+      id: values.id,
+      name: values.name,
+      cost: values.cost,
+      category: values.category,
+    };
+    api.post("/api/games", addPost);
   };
 
   useEffect(() => {
-    Axios.get("http://localhost:3001/getCards").then((response) => {
+    Axios.get("http://localhost:3333/api/games").then((response) => {
       setListCard(response.data);
     });
   }, []);
-
-  const handleaddValues = (value) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [value.target.name]: value.target.value,
-    }));
-  };
 
   return (
     <div className="app-container">
       <div className="register-container">
         <h1 className="register-title">Scrim Shop</h1>
+        <Formik
+          // validationSchema={schema}
+          initialValues={{
+            name: "",
+            cost: "",
+            category: "",
+          }}
+          onSubmit={(values) => handleSubmit(values)}
+        >
+          {({ values, errors, touched, handleChange }) => {
+            return (
+              <Form>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Nome"
+                  className="register-input"
+                  onChange={handleChange}
+                  value={values.name}
+                />
+                <input
+                  type="text"
+                  placeholder="Preço"
+                  name="cost"
+                  className="register-input"
+                  onChange={handleChange}
+                  value={values.cost}
+                />
+                <input
+                  type="text"
+                  placeholder="Categoria"
+                  name="category"
+                  className="register-input"
+                  onChange={handleChange}
+                  value={values.category}
+                />
 
-        <input
-          type="text"
-          name="name"
-          placeholder="Nome"
-          className="register-input"
-          onChange={handleaddValues}
-        />
-        <input
-          type="text"
-          placeholder="Preço"
-          name="cost"
-          className="register-input"
-          onChange={handleaddValues}
-        />
-        <input
-          type="text"
-          placeholder="Categoria"
-          name="category"
-          className="register-input"
-          onChange={handleaddValues}
-        />
-
-        <button onClick={handleRegisterGame} className="register-button">
-          Cadastrar
-        </button>
+                <button type="submit" className="register-button">
+                  Cadastrar
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
       </div>
 
       {listCard?.map((val) => (
         <Card
-          listCard={listCard}
-          setListCard={setListCard}
           key={val.id}
           id={val.id}
           name={val.name}
           cost={val.cost}
           category={val.category}
+          listCard={[
+            {
+              name: "",
+              cost: "",
+              category: "",
+              setValues: null,
+              id: 0,
+            },
+          ]}
+          setListCard={()=>{}}
         />
       ))}
     </div>
